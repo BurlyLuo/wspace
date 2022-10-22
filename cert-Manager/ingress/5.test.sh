ef3905bfc930da1e86a682dcb9255e8d288b79e1
BurlyLuo <olaf.luo@foxmail.com>
2022-10-22 19:32:56 +0800

    date

--- cert-Manager/ingress/5.test.sh
+++ cert-Manager/ingress/5.test.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+set -v
 lb_ip=`kubectl -nsandbox get svc --no-headers | grep ingress-nginx-controller | grep LoadBalancer |awk -F  " " '{print $4}'`
+sed -i '/https-example.foo.com/d' /etc/hosts > /dev/null
+
 echo $lb_ip https-example.foo.com >> /etc/hosts
+
 while true;do curl -k https://https-example.foo.com;sleep 1;done
+
