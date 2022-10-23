183aae057abc940fd341d92a5e9b2ad9bc9d91a7
BurlyLuo <olaf.luo@foxmail.com>
2022-10-23 21:59:29 +0800

    add flannel ipsec mode

---
+++ FLANNEL/flannel-ipsec/1-setup-flannel-ipsec.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -v
+
+kind create cluster --name=flannel-ipsec --config=./kind-flannel.yaml --image=kindest/node:v1.23.4
+
+controller_node=$(kubectl get nodes --no-headers  -o custom-columns=NAME:.metadata.name| grep control-plane)
+kubectl taint nodes $controller_node node-role.kubernetes.io/master:NoSchedule-
+
+kubectl apply -f ./flannel.yaml
+
+for i in $(docker ps  -a --format "table {{.Names}}"|grep flannel-ipsec );do echo $i;docker cp ./bridge $i:/opt/cni/bin/;docker cp /usr/bin/ping $i:/usr/bin/ping;docker exec -it $i  bash -c "sed -i -e 's/jp.archive.ubuntu.com\|archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list";docker exec -it $i bash -c "apt-get -y update >/dev/null && apt-get -y install net-tools tcpdump lrzsz >/dev/null";done
+
